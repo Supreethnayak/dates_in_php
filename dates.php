8f52e14d8649dbe72298ac2f2027b0de0442e431
Supreethnayak <59947774+Supreethnayak@users.noreply.github.com>
2020-07-01 23:04:19 +0530

    Add files via upload

---
+++ dates.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Date in php</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <br>
+    <aside></aside>
+    <div>
+        <?php
+                echo date('d M Y '); 
+        ?>
+        <ul>
+            <a class="Iknow" href="index.html">  OK</a>   
+        </ul>
+    </div>
+</body>
+</html>
